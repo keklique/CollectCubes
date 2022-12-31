d93cbb320a71a20eadac3f0c9610132b77df700e
keklique <41877582+keklique@users.noreply.github.com>
2023-01-01 00:16:21 +0300

    LevelType created

---
+++ Assets/Scripts/Enums/LevelType.cs
@@ -0,0 +1,13 @@
+// LevelType by Ahmet Keklik
+// e-mail: ahmetkeklik@outlook.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LevelType 
+{
+    Standard,
+    Rival,
+    Timer
+}
