54586e32939bd0db2e26f616d1e802dec366cc4a
keklique <41877582+keklique@users.noreply.github.com>
2022-12-31 17:05:40 +0300

    ICollectable interface added

---
+++ Assets/Scripts/Interfaces/ICollectable.cs
@@ -0,0 +1,13 @@
+// ICollectable by Ahmet Keklik
+// e-mail: ahmetkeklik@outlook.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICollectable
+{
+    void Activate();
+    void Collect();
+    void Instantiate();
+}
