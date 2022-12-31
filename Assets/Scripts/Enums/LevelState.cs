c6ccb3a66ffc72260b2eefd91ab6e53c3daf32b9
keklique <41877582+keklique@users.noreply.github.com>
2023-01-01 00:08:44 +0300

    LevelState created

---
+++ Assets/Scripts/Enums/LevelState.cs
@@ -0,0 +1,14 @@
+// LevelState by Ahmet Keklik
+// e-mail: ahmetkeklik@outlook.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LevelState 
+{
+	Init,
+	Play,
+	Success,
+	Fail
+}
