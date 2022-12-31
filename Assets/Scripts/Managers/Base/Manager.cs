a1584cc94d6af02494a73022fbcb896386726d72
keklique <41877582+keklique@users.noreply.github.com>
2022-12-31 17:04:08 +0300

    Menager abstract class and levelmanager created

---
+++ Assets/Scripts/Managers/Base/Manager.cs
@@ -0,0 +1,26 @@
+// Manager by Ahmet Keklik
+// e-mail: ahmetkeklik@outlook.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Manager<T> : MonoBehaviour 
+{
+	
+    #region UNITY_EVENTS
+
+    #endregion
+
+    #region EVENTS
+
+    #endregion
+
+    #region PUBLIC_METHODS
+
+    #endregion
+
+    #region PRIVATE_METHODS
+
+    #endregion
+}
---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,38 @@
+// LevelManager by Ahmet Keklik
+// e-mail: ahmetkeklik@outlook.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : Manager<LevelManager>
+{
+	
+    [Header("Level Design")]
+    private int temp1;
+
+    [Space(15)]
+    [Header("General Variables")]
+    private int temp2;
+
+    [Space(15)]
+    [Header("References")]
+    private int temp3;
+
+
+    #region UNITY_EVENTS
+
+    #endregion
+
+    #region EVENTS
+
+    #endregion
+
+    #region PUBLIC_METHODS
+
+    #endregion
+
+    #region PRIVATE_METHODS
+
+    #endregion
+}
