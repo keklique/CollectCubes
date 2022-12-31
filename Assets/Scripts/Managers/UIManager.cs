35909790ef8c16487547fb115d3a72fcbbcd83b6
keklique <41877582+keklique@users.noreply.github.com>
2022-12-31 19:57:39 +0300

    TMP imported

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,29 @@
+// UIManager by Ahmet Keklik
+// e-mail: ahmetkeklik@outlook.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : Manager<UIManager>
+{
+	
+    
+
+
+    #region UNITY_EVENTS
+
+    #endregion
+
+    #region EVENTS
+
+    #endregion
+
+    #region PUBLIC_METHODS
+
+    #endregion
+
+    #region PRIVATE_METHODS
+
+    #endregion
+}
